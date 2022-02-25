56faa42c4508b9857a00a5e44f46569ed8591d74
Narnillian <ner.baby@gmail.com>
2022-02-25 13:53:41 -0800

    Finish v1 of loader, second version is planned

--- loader.cpp
+++ loader.cpp
@@ -2,59 +2,61 @@
 #include <time.h>
 using namespace std;
 
+int moveback;
+int characters;
+int writeChars;
 
-int wait(struct timespec ts) {
+
+int printwait(struct timespec ts) {
   fflush(stdout);
   nanosleep(&ts,NULL);
   return 0;
 }
 
+
+void v1(struct timespec ts, int limit = 0) {
+  int iterations = 0;
+  bool checker = true;
+  if (limit) {
+    checker = false;
+  }
+  while (checker || iterations < limit) {
+    iterations++;
+    ts.tv_nsec=20000000;
+    characters = 15;
+    for (writeChars=0; writeChars < characters; writeChars++) {
+      cout << "|";
+      printwait(ts);
+    }
+    cout << "\033[s";
+    nanosleep(&ts,NULL);
+    for (writeChars=0; writeChars < characters; writeChars++) {
+      moveback = writeChars+1;
+      cout << "\033[u\033[" << moveback << "D\\";
+      printwait(ts);
+    }
+    cout << "\033[1D";
+    printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);
+    //cout << iterations << "\033[2D";
+    printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);printwait(ts);
+    ts.tv_nsec=70000000;
+    printwait(ts);
+  }
+
+}
+
 int main() {
-
-  int moveback;
-  int characters;
-  int writeChars;
-
-  struct timespec ts;
-  ts.tv_nsec=20000000;
-  ts.tv_sec=0;
   
+  struct timespec ts;
+  ts.tv_sec=0;
+
+
   cout << "Loading...   ";
-  characters = 15;
-  for (writeChars = 0; writeChars < characters; writeChars++) {
-    cout << "|";
-    wait(ts);
-  }
-  cout << "\033[s";
-  nanosleep(&ts,NULL);
-  for (writeChars = 0; writeChars < characters; writeChars++) {
-    moveback = writeChars-1;
-    printf("\033u\033[%dD\\", moveback);
-    wait(ts);
-  }
+
+  v1(ts,10);
 
   cout << "\n";
   
 
   return 0;
 }
-
-
-/*
-int pipes=11;
-
-  //begin pipe setup
-  printf(" ");
-  for (i=0; i<pipes+1; i++) {
-    printf("| ");
-    sleep(2);
-  }
-
-  //begin animation
-  for (i=0; i<pipes+1; i++) {
-    printf("\033[7");
-  }
-  printf("\n");
-
-
-*/
